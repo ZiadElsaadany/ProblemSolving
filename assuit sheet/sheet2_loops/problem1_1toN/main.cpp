c8705d38798c54b4b2760814e069e4ad3ef51a99
ZiadElsaadany <ziadelsaadany73@gmail.com>
2022-05-13 20:05:08 +0200

    problem1 done

---
+++ assuit sheet/sheet2_loops/problem1_1toN/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std ;
+int main(){
+    //sheet 2 assuit
+    // problem 1  1 to N
+
+int n;
+cin >> n;
+for(int i = 1 ; i<=n ;i++){
+    cout << i <<endl ;
+}
+}
+
