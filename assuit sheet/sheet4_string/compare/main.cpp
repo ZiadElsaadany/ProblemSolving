a64c84b3a9a3cd43b84df98f96aef20971b577b2
ZiadElsaadany <ziadelsaadany73@gmail.com>
2022-05-29 14:50:37 +0200

    compare done

--- assuit sheet/sheet4_string/compare/main.cpp
+++ assuit sheet/sheet4_string/compare/main.cpp
@@ -1,9 +1,30 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
+#include <string.h>
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    char z[21]    ;
+    char n[21] ;
+    cin.getline(z,21);
+    cin.getline(n,21);
+
+   if(strcmp(z,n)==0){
+    cout << z <<endl;
+   }else if(strcmp(z,n)== 1 ){
+   cout <<  n <<endl ;
+   }
+   else{
+        cout << z  <<endl;
+
+   }
+
+
     return 0;
 }
+ /**
+ input
+ acm
+acpc
+
+ **/
