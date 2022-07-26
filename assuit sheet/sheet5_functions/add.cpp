1645deb8140fa4c3862ec0f70fdeaa12cb35a0de
ZiadElsaadany <ziadelsaadany73@gmail.com>
2022-07-26 18:07:14 +0200

    add added

--- assuit sheet/sheet5_functions/add.cpp
+++ assuit sheet/sheet5_functions/add.cpp
@@ -1,23 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
 
-#include <bits/stdc++.h>
-using namespace std ;
-void print  (  int number ) {
-
-void addd(int num1 , int num2) {
- cout  << num1+num2 ;
-}
-for  (int  i = 0 ; i < number  ; i ++ ) {
-        if(i == number -1) {
-            cout  << i + 1 ;
-        }else {
-        cout  << i + 1 <<" " ;
-        }
+void add(int number1 ,int number2) {
+cout << number1+number2  <<endl;
 
 }
-}
-int main ( ) {
-    int number1 ;
-    cin >> number1 ;
-    print(number1) ;
-    addd(1,2) ;
+
+int main(){
+int num1 , num2 ;
+cin >>num1 >>num2 ;
+add(num1,num2) ;
 }
