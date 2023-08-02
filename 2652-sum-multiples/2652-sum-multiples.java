43391113c7edb5b8ebeca7f5b47dba83a709dceb
Ziad Elsdaadany <101086229+ZiadElsaadany@users.noreply.github.com>
2023-08-02 14:56:36 +0300

    Time: 2 ms (98.33%), Space: 40.1 MB (22.95%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.java
+++ 2652-sum-multiples/2652-sum-multiples.java
@@ -3,14 +3,12 @@ class Solution {
 
 int sum= 0 ;          
 
-   for( int i =1 ; i<=n ;i++  ) {  
+   for( int i =3 ; i<=n ;i++  ) {  
                
     if(i%3==0||i%5==0||i%7==0)  {  
  sum+=i ; 
 }
-
-
-        }
+  }
 return sum ;
 
     }
