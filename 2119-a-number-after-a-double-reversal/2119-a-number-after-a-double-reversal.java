d65b93c9350c8c523a74015dc679bd7c80a8a69a
Ziad Elsdaadany <101086229+ZiadElsaadany@users.noreply.github.com>
2023-08-02 15:52:15 +0300

    Time: 0 ms (100.00%), Space: 40.1 MB (11.07%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
@@ -10,18 +10,40 @@ class Solution {
          // 12006
 
 // 12006
-String s=Integer.toString(num);  
-if(s.length() == 1 && s.equals( "0") ) { 
-return true;  
-}else{
+// 13%3 = 1
+// 12%3 = 0
+// 1000 %2 = 0 
+// 1200 %2=0 
+// 1200 % 3= 0
+// 1800 %9 = 0
+// 1800%10 = 0
+// 
+
+if(num != 0 &&  num%10 == 0 ) {  
+return false;
+}
+return true; 
+
+
+
+
+
+
+
+
+
+// String s=Integer.toString(num);  
+// if(s.length() == 1 && s.equals( "0") ) { 
+// return true;  
+// }else{
     
-   if(s.charAt(s.length()-1) != '0') {
-  return true; 
- }
-else{   
-return  false;
-}  
- }
+//    if(s.charAt(s.length()-1) != '0') {
+//   return true; 
+//  }
+// else{   
+// return  false;
+// }  
+//  }
    
  }
      }
