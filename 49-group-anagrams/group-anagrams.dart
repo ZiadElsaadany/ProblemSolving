c89a493b5393009d15a6b55fe6b73e60ab7babbd
Ziad Elsaadany <101086229+ZiadElsaadany@users.noreply.github.com>
2025-06-12 21:09:23 +0300

    Time: 302 ms (9.37%) | Memory: 154.4 MB (97.92%) - LeetSync

--- 49-group-anagrams/group-anagrams.dart
+++ 49-group-anagrams/group-anagrams.dart
@@ -11,27 +11,21 @@ class Solution {
      strsIndividualSorted.add(z.join());
     i++ ;
   }
-  
   List<List<String>>   strsGroupSorted = [] ; 
   Set<int> visited = {};
-
   for(int i = 0 ; i<strsIndividualSorted.length; i++ ) { 
    if (visited.contains(i)) continue;
     String a= strsIndividualSorted[i]; 
      List<String>   z = [] ;
-   for(int  j = i  ;  j < strsIndividualSorted.length; j++ ){ 
+   for(int   j =i  ;  j < strsIndividualSorted.length; j++ ){ 
    
    if(a== strsIndividualSorted[j]) {  
   z.add(strs[j]);
      visited.add(j);
    }
    }
-    
-      
     strsGroupSorted.add(z);
-   
   }
-  
   return strsGroupSorted;
   }
   
